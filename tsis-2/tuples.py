00f258908a777d70efcfd681423095c6af5cbcdf
DamirMayor <156983125+DamirMayor@users.noreply.github.com>
2024-01-28 02:03:49 +0600

    Update tuples.py

--- tsis-2/tuples.py
+++ tsis-2/tuples.py
@@ -16,3 +16,36 @@ print(fruits[-1])
 #4
 fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(fruits[2:5])
+
+
+#Examples
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
+print(thistuple[1])
+print(thistuple[-1])
+print('\n')
+x = ("rabbit", "fox", "wolf")
+y = list(x)
+y[1] = "kolobok"
+x = tuple(y)
+print(x)
+y = ("elephant",)
+x += y
+print(x)
+y = list(x)
+y.remove("wolf")
+x = tuple(y)
+print(x)
+(odin, dva, tri) = x
+print(odin)
+print(dva)
+print(tri)
+for y in x:
+  print(y)
+mytuple = x * 2
+print(mytuple)
