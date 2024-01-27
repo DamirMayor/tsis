64e928a4565fc22e649109f71af2ccfd9210bf41
DamirMayor <156983125+DamirMayor@users.noreply.github.com>
2024-01-28 00:52:54 +0600

    Update operators.py

--- tsis-2/operators.py
+++ tsis-2/operators.py
@@ -20,3 +20,19 @@ if 5 != 10:
 #5
 if 5 == 10 or 4 == 4:
   print("At least one of the statements is true")
+
+
+#Examples
+print(7 + 8)
+print(10 - 4)
+print(2*5)
+print(8/4)
+print(2**6)
+print(109%5)
+print(25//6)
+print('\n')
+x = 5
+x += 7
+print(x)
+print(49 + 8 * 6)
+print(4 + 8 - 1)
