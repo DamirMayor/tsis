78363b5ba96f5a5c81dc0f31c1d1e57acfe029c9
DamirMayor <156983125+DamirMayor@users.noreply.github.com>
2024-02-01 09:11:36 +0600

    Update lambda.py

--- tsis-3/classes/lambda.py
+++ tsis-3/classes/lambda.py
@@ -1,4 +1,16 @@
 #1
+x = lambda a : a
 
 
 #Examples
+x = lambda a : a + 10
+print(x(5))
+x = lambda a, b : a * b
+print(x(5, 6))
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 7))
+print('\n')
+def my_func(n):
+  return lambda a : a * n
+my_func1 = my_func(2)
+print(my_func(11))
