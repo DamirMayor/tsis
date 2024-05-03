0792f097e714f65ad92884fcff6b6483b31d4c05
DamirMayor <156983125+DamirMayor@users.noreply.github.com>
2024-05-03 19:03:33 +0500

    Update and rename phonebook.py to phonebook_func.py

---
+++ tsis-11/phonebook_func.py
@@ -0,0 +1,18 @@
+import sqlite3 as sq
+
+
+with sq.connect('phonebook.db') as con:
+  cur = con.cursor()
+  cur.execute("CREATE TABLE IF NOT EXISTS phonebook(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)")
+
+  
+  # cur.execute("SELECT * FROM phonebook WHERE name LIKE 'Jo%'")
+  # print(cur.fetchall())
+  # cur.execute("SELECT * FROM phonebook WHERE phone LIKE '+7747%'")
+  # print(cur.fetchall())
+
+
+  # cur.execute("SELECT * FROM phonebook LIMIT 2, 2 ")
+  # print(cur.fetchall())
+
+ 
