f0ddbcf5ea9e0034f5890cf8903829ac403d4cfe
DamirMayor <156983125+DamirMayor@users.noreply.github.com>
2024-05-03 21:08:04 +0500

    Update phonebook_func.py

--- tsis-11/phonebook_func.py
+++ tsis-11/phonebook_func.py
@@ -5,14 +5,16 @@ with sq.connect('phonebook.db') as con:
   cur = con.cursor()
   cur.execute("CREATE TABLE IF NOT EXISTS phonebook(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)")
 
+  #1
   
-  # cur.execute("SELECT * FROM phonebook WHERE name LIKE 'Jo%'")
-  # print(cur.fetchall())
-  # cur.execute("SELECT * FROM phonebook WHERE phone LIKE '+7747%'")
-  # print(cur.fetchall())
+  cur.execute("SELECT * FROM phonebook WHERE name LIKE 'Jo%'")
+  print(cur.fetchall())
+  cur.execute("SELECT * FROM phonebook WHERE phone LIKE '+7747%'")
+  print(cur.fetchall())
 
+  #4
 
-  # cur.execute("SELECT * FROM phonebook LIMIT 2, 2 ")
-  # print(cur.fetchall())
+  cur.execute("SELECT * FROM phonebook LIMIT 2, 2 ")
+  print(cur.fetchall())
 
  
